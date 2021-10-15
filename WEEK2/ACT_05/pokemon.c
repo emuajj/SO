27204bfc13f6a34022ea19ff8d07a0992dd060de
JordiMateoUdL <jordi.mateo@udl.cat>
2021-10-15 14:40:02 +0200

    Solucio act5

--- WEEK2/ACT_05/pokemon.c
+++ WEEK2/ACT_05/pokemon.c
@@ -11,39 +11,11 @@ double height;
 double weight;
 };
 
-Pokemon create_pokemon()
-{
-    FILE* f = fopen("./pokedex.csv", "w");
-    Pokemon newP;
-    
-    printf("Enter pokemon id: ");
-    scanf("%d", newP.pokemon_id);
-
-    printf("Enter pokemon name: ");
-    scanf("%s", newP.pokemon_name);
-
-    printf("Enter pokemon height: ");
-    scanf("%lf", newP.pokemon_height);
-
-    printf("Enter pokemon weight: ");
-    scanf("%d", newP.pokemon_weight);
-
-    sprintf(f, newP.pokemon_id()),strlen(newP.pokemon_id());
-    sprintf(f, ",");
-    sprintf(f, newP.pokemon_name(),strlen(newP.pokemon_name()));
-    sprintf(f, ",");
-    sprintf(f, newP.pokemon_height(),strlen(newP.pokemon_height()));
-    sprintf(f, ",");
-    sprintf(f, newP.pokemon_weight(),strlen(newP.pokemon_weight()));
-    sprintf(f, "\n");
-}
-
-
 Pokemon new_pokemon(int pokemon_id, char *name, double height,double weight)
 {  
     Pokemon pokemon = malloc(sizeof(struct pokemon));
     pokemon->pokemon_id = pokemon_id;
-    pokemon->name = name;
+    pokemon->name = strdup(name);
     pokemon->height = height;
     pokemon->weight = weight;
 
